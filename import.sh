9b55c851d120ff921678a5790d24b2d6ec9cd300
tanjo <tanjo@users.noreply.github.com>
2019-11-11 16:47:27 +0900

    シンボリックリンクだとなんかだめだった

--- import.sh
+++ import.sh
@@ -4,7 +4,7 @@ read ANS
 case $ANS in
   [Yy]* )
         trash ~/Library/Application\ Support/Code/User/settings.json
-        ln -s ./settings.json ~/Library/Application\ Support/Code/User/settings.json
+        cp ./settings.json ~/Library/Application\ Support/Code/User/settings.json
         ;;
   * )
         ;;
@@ -14,7 +14,7 @@ read ANS
 case $ANS in
   [Yy]* )
         trash ~/Library/Application\ Support/Code/User/keybindings.json
-        ln -s ./keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
+        cp ./keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
         ;;
   * )
         ;;
